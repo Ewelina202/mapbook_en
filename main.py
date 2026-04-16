37db63f03925bbaf0a415e1f055366e1a8ddd48a
Ewelina202 <ewelina.nowak01@student.wat.edu.pl>
2026-04-16 14:00:33 +0200

    wip: mvc template applied

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-users: list = [
-    {'username': 'oliwia', 'location': 'łódz', 'posts': 1,
-     'usermassage': ['życzenia1', 'kocham legie', 'sprzedam opla', 'kiwi']},
-    {'username': 'pawel', 'location': 'ostróda', 'posts': 2,
-     'usermassage': ['życzenia2', 'kocham legie1', 'sprzedam opla1', ]},
-    {'username': 'eliza', 'location': 'łódz', 'posts': 3, 'usermassage': ['życzenia3', 'kocham legie2']},
-    {'username': 'filip', 'location': 'deblin', 'posts': 4,
-     'usermassage': ['życzenia4', 'kocham legie3', 'sprzedam opla', 'kiwi']},
-]
+from mapbook_lib.model import users
+from mapbook_lib.controller import read_data
 
 
-def read_data(users_data: list) -> None:
-    for user in users_data:
-        print(
-            f'twój znajomy {user['username']} z miejscowości {user['location']} opubikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermassage'][-1]}')
+
 
 
 read_data(users[1:])

2f184bfcc9d956bfaa983effe0b4f2f9bd85c95b
Ewelina202 <ewelina.nowak01@student.wat.edu.pl>
2026-04-16 13:52:11 +0200

    wip: simple structure of users list

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-
-
 users: list = [
-    {'username': 'oliwia','location':'łódz','posts':1,'usermassage':['życzenia1','kocham legie', 'sprzedam opla','kiwi']},
-    {'username': 'pawel','location':'ostróda','posts':2,'usermassage':['życzenia2','kocham legie1', 'sprzedam opla1',]},
-    {'username': 'eliza','location':'łódz','posts':3,'usermassage':['życzenia3','kocham legie2']},
-    {'username': 'filip','location':'deblin','posts':4,'usermassage':['życzenia4','kocham legie3','sprzedam opla','kiwi']},
+    {'username': 'oliwia', 'location': 'łódz', 'posts': 1,
+     'usermassage': ['życzenia1', 'kocham legie', 'sprzedam opla', 'kiwi']},
+    {'username': 'pawel', 'location': 'ostróda', 'posts': 2,
+     'usermassage': ['życzenia2', 'kocham legie1', 'sprzedam opla1', ]},
+    {'username': 'eliza', 'location': 'łódz', 'posts': 3, 'usermassage': ['życzenia3', 'kocham legie2']},
+    {'username': 'filip', 'location': 'deblin', 'posts': 4,
+     'usermassage': ['życzenia4', 'kocham legie3', 'sprzedam opla', 'kiwi']},
 ]
 
-for user in users:
-    print(f'twój znajomy {user['username']} z miejscowości {user['location']} opubikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermassage'][-1]}')
-    
-    # twój znajomy filip z miejscowości deblin opublikował 1 post o treści: życzenia
+
+def read_data(users_data: list) -> None:
+    for user in users_data:
+        print(
+            f'twój znajomy {user['username']} z miejscowości {user['location']} opubikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermassage'][-1]}')
+
+
+read_data(users[1:])
